8d3388edc3ea3010c6a8be516ce2b8d335c09f76
alibeep1 <ali_90082@aucegypt.edu>
2024-04-22 09:10:21 +0200

    cleaned grid mngr script

--- Assets/GridManager.cs
+++ Assets/GridManager.cs
@@ -14,30 +14,38 @@ public class GridManager : MonoBehaviour
     [SerializeField] GameObject occupied_seat_prefab = null;
 
     [SerializeField] int empty_to_occupied_ratio = 2;
+    [SerializeField] int babyOffset = -14;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        //  QUESTION: in the very near future, how would we link the instantiated occupied with a passenger entity (annoyance, etc., interactions, or scripts, more generall)
         if (empty_seat_prefab != null)
         {
-            //int gridCells = numOfColumns * numOfRows;
             for (int i = 0; i < numOfRows; i++)
             {
                 for (int j = 0; j < numOfColumns; j++)
                 {
                     if ((j < columns_left || j >= numOfColumns - columns_right) && i != 4)
                     {
-                        int rand = Random.Range(1, empty_to_occupied_ratio + 1);
-
-                        // If the random number is 1, instantiate an occupied seat, otherwise instantiate an empty seat
-                        GameObject seatToInstantiate = (rand == 1) ? occupied_seat_prefab : empty_seat_prefab;
-
-                        // the -14 below changes with the setup of the above variables;
-                        // It is defined for this combo: 8,10, Baby(Transform), 4,5,3,3, seat_empty (prefab)
-                        Instantiate(seatToInstantiate, new Vector3((x_space * j) - 14, (y_space * i)), Quaternion.identity);
+                        InstantiateSeat(j, i);
                     }
 
                 }
             }
         }
+    }
+
+    private void InstantiateSeat(int column, int row)
+    {
+        int rand = Random.Range(1, empty_to_occupied_ratio + 1);
+
+        // If the random number is 1, instantiate an occupied seat, otherwise instantiate an empty seat
+        GameObject seatToInstantiate = (rand == 1) ? occupied_seat_prefab : empty_seat_prefab;
+
+        // the -14 below changes with the setup of the above variables;
+        // It is defined for this combo: 8,10, Baby(Transform), 4,5,3,3, seat_empty (prefab)
+        Instantiate(seatToInstantiate, new Vector3((x_space * column) + babyOffset, (y_space * row)), Quaternion.identity);
     }
 }

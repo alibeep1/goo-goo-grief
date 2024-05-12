497743cd972aaf90bcf384d37671f5b0d9b63382
andrewsinout <70208780+andrewsinout@users.noreply.github.com>
2024-05-12 23:03:24 +0300

    Seperating Mechanics Classes and Fixing Pee Mechanism

---
+++ Assets/Script/Gameplay/FartMechanism.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FartMechanism : MonoBehaviour
+{
+    public ParticleSystem particlesystem;
+    [SerializeField] InputActionManager inputActions = null;
+    [SerializeField] bool fartEnable = false;
+    [SerializeField] ProgressBar prog;
+
+    private void Start()
+    {
+        // If particlesystem reference is not assigned, try to get it from the GameObject
+        if (particlesystem == null)
+        {
+            particlesystem = GetComponent<ParticleSystem>();
+        }
+
+        if (inputActions != null)
+        {
+            inputActions.RegisterToButtonEvents(Button_listener);
+        }
+    }
+
+
+    void Button_listener(IInputAction<bool> i_value, KeyCode i_keyCode)
+    {  
+        if (i_keyCode == KeyCode.F)
+        {
+            Fart_handler();
+        }
+    }
+    void Fart_handler()
+    {
+        Vector2 particlesystemposition = transform.position + Vector3.up * 1.4f;
+        particlesystemposition.y = particlesystemposition.y - 2.5f;
+        
+        Vector3 rotation = transform.rotation.eulerAngles;
+        Debug.Log(rotation.z);
+        if (rotation.z == 90) {
+            particlesystem.transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        else if (rotation.z == 270) {
+            particlesystem.transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+        else if (rotation.z == 180) {
+            particlesystem.transform.rotation = Quaternion.Euler(270, 0, 0);
+        } else {
+            particlesystem.transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
+        // particlesystem.transform.rotation.x = transform.rotation;
+        particlesystem.transform.position = particlesystemposition;
+        var em = particlesystem.emission;
+        var dur = particlesystem.duration;
+        em.enabled = true;
+        prog.IncProgressBar(0.05f);
+        particlesystem.Play();
+        StartCoroutine(BabyFartDuration(1.0f));
+    }
+
+    // void DestroyObj () {
+    //     Destroy (gameObject);
+    // }
+
+    IEnumerator BabyFartDuration(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        var em = particlesystem.emission;
+        em.enabled = false;
+    }
+
+}
+
+ 
+// public bool once = true;
+// private void OnTriggerEnter2D (Collider2D other) {
+// if (other.gameObject.CompareTag ("Player") && once) {
+// var em = collisionParticleSystem.emission;
+// var dur = collisionParticlesystem duration;
+// em.enabled = true;
+// collisionParticleSystem. Play () ;
+// once = false;
+// Destroy (sr);
+// Invoke (nameof (DestroyObj), dur);
---
+++ Assets/Script/Gameplay/PeeMechanism.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeeMechanism : MonoBehaviour
+{
+
+    [SerializeField] InputActionManager inputActions = null;
+    [SerializeField] float pee_distance = 20f;
+
+    [SerializeField] GameObject missilePrefab = null;
+    [SerializeField] float missileSpeed = 0.7f;
+    [SerializeField] ProgressBar prog;
+
+    private void Start()
+    {
+        if (inputActions != null)
+        {
+            inputActions.RegisterToButtonEvents(Button_listener);
+        }
+    }
+   void Button_listener(IInputAction<bool> i_value, KeyCode i_keyCode)
+    {
+        if (i_keyCode == KeyCode.P)
+        {
+            Pee_handler();
+        }
+    }
+
+    void Pee_handler()
+    {
+        Vector2 rayOrigin = transform.position + Vector3.up * 1.4f;
+        Debug.Log($"rayOrigin: {rayOrigin}");
+
+
+        // Instantiate the Pee prefab
+        GameObject missileObject = Instantiate(missilePrefab, transform.position, Quaternion.identity);
+
+        Rigidbody2D missile = missileObject.GetComponent<Rigidbody2D>();
+
+        Vector2 direction = transform.up.normalized;
+
+        if (missile != null)
+        {
+            // Launch the Pee object as a missile in the direction of the baby's rotation and the given speed
+            missile.AddForce(direction * missileSpeed, ForceMode2D.Impulse);
+        }
+        prog.IncProgressBar(0.05f);
+
+    }
+}

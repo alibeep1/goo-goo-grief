72a16be7a6f9d67386a872612b8deae1687c8af7
andrewsinout <70208780+andrewsinout@users.noreply.github.com>
2024-05-19 16:04:08 +0300

    Baby Animation

--- Assets/Script/Gameplay/BabyMovement.cs
+++ Assets/Script/Gameplay/BabyMovement.cs
@@ -1,66 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BabyMovement : MonoBehaviour
 {
+    [SerializeField] private InputActionManager inputActions = null;
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float rotationSpeed = 1.0f;
+    [SerializeField] private float inputThreshold = 0.1f; // Threshold to ignore small input values
 
-    [SerializeField] InputActionManager inputActions = null;
-    [SerializeField] float speed = 1.0f;
-    [SerializeField] float rotationSpeed = 1.0f;
-
+    private Animator animator;
     private Vector3 moveInput = Vector3.zero;
 
     private void Start()
     {
         // Subscribe to Axis change events
         inputActions.RegisterToAxisEvents(Axis_Listener);
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Axis_Listener(IInputAction<Vector2> i_value)
-    {
+void Axis_Listener(IInputAction<Vector2> i_value)
+{
+    Vector2 axes = inputActions.GetAxisData().GetValue().Value;
 
-        IInputAction<Vector2> axes = inputActions.GetAxisData();
+    Debug.Log($"Axes: {axes}");
 
-        moveInput.x = axes.GetValue().Value.x * Time.deltaTime * speed;
+    // Calculate movement input
+    moveInput.x = axes.x * speed * Time.deltaTime;
+    moveInput.y = axes.y * speed * Time.deltaTime;
 
-        moveInput.y = axes.GetValue().Value.y * Time.deltaTime * speed;
+        if (Mathf.Abs(axes.x) > inputThreshold || Mathf.Abs(axes.y) > inputThreshold)
+        {
+            move(moveInput);
+            rotate(moveInput);
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+}
 
-        move(moveInput);
-
-        rotate(moveInput);
-    }
 
     private void rotate(Vector3 i_movement)
     {
-        moveInput.Normalize();
-
+        i_movement.Normalize();
         Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, i_movement);
-
         transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
     }
 
-
-    // We just increment the transform's position by the given delta
-    void move(Vector3 i_deltaPosition)
+    private void move(Vector3 i_deltaPosition)
     {
         Vector3 position = transform.position;
         position += i_deltaPosition;
         transform.position = position;
     }
-
-    //void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    var collidedObject = collision.gameObject;
-    //    var collider = collision.collider;
-
-    //    Debug.Log($"Baby collided with {collidedObject.name}");
-
-    //    if (collidedObject.name == "occupied_seat(Clone)" && collider is CircleCollider2D)
-    //    {
-    //        Debug.Log("Collided with baby's circle collider...");
-    //        return;
-    //    }
-    //}
 }
